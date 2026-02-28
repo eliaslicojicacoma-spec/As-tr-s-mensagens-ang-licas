a247c16daf16a85994d507f1bf955a81a60e75f6
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-01 00:13:21 +0100

    Create siteConfig.ts

---
+++ src/config/siteConfig.ts
@@ -0,0 +1,15 @@
+export const siteConfig = {
+  name: "As Três Mensagens Angélicas",
+  author: "Elias Licoji Cacoma",
+  mission: "Proclamar o Evangelho Eterno e preparar um povo para o breve retorno de Cristo.",
+  location: "Angola",
+  social: {
+    youtube: "https://youtube.com/@eliaslicojicacoma",
+    instagram: "https://www.instagram.com/eliascacoma",
+    facebook: "https://www.facebook.com/share/1BDeKzYbTU/"
+  },
+  monetization: {
+    pix: "elias-licoji-cacoma-273@jim.com",
+    paypal: "eliaslicojicacoma@gmail.com"
+  }
+};
