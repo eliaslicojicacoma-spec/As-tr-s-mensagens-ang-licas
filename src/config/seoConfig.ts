0fe4c542a56195bb62877b79e45bd6bcd5e3c37e
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-01 01:11:51 +0100

    Create seoConfig.ts

---
+++ src/config/seoConfig.ts
@@ -0,0 +1,20 @@
+export const seoConfig = {
+  defaultTitle: "As Três Mensagens Angélicas",
+  titleTemplate: "%s | Clarim da Verdade",
+  description: "Portal missionário dedicado à proclamação do Evangelho Eterno direto de Angola para o mundo.",
+  openGraph: {
+    type: 'website',
+    locale: 'pt_AO',
+    url: 'https://as-tres-mensagens-angelicas.com/',
+    site_name: 'Clarim da Verdade',
+    images: [
+      {
+        url: '/images/og/og-default.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Clarim da Verdade',
+      },
+    ],
+  }
+};
+
