b4ba21ffafc9273e286755d1a655757b39e4a371
eliaslicojicacoma-spec <eliaslicojicacoma@gmail.com>
2026-03-01 01:27:43 +0100

    Create useTheme.ts

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+export const useScrollPosition = () => {
+  const [scrollPos, setScrollPos] = useState(0);
+
+  useEffect(() => {
+    const updatePosition = () => {
+      setScrollPos(window.pageYOffset);
+    };
+    window.addEventListener("scroll", updatePosition);
+    updatePosition();
+    return () => window.removeEventListener("scroll", updatePosition);
+  }, []);
+
+  return scrollPos;
+};
+
